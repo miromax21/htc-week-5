1ba4adf09b3432f0815410360a3ec3baf0476676
miromax <miromax21@gmail.com>
2019-04-23 12:46:03 +0400

    Initial Commit

---
+++ htc-week-5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  htc-week-5
+//
+//  Created by maxim mironov on 23/04/2019.
+//  Copyright © 2019 maxim mironov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
